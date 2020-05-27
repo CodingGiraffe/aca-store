60ba723f5ebdfad8fd1cf28dee4f37ccdd080155
CodingGiraffe <adamthomas03@gmail.com>
2020-05-26 20:23:47 -0500

    close but not there

--- public/index.js
+++ public/index.js
@@ -1,2 +1,31 @@
-//stuff
-//more stuff
+'use strict'
+
+console.log("loading script.js file")
+
+window.onload = function() {
+    console.log("window loaded")
+    getInfo()
+}
+
+let getInfo = function() {
+    console.log("Inside the post method, about to make a fetch request")
+    let fetchPromise = fetch('https://my-json-server.typicode.com/jubs16/Products/Products')
+
+    let dataPromise = fetchPromise.then(function(response) {
+        console.log(response)
+        return response.json()
+    })
+
+    dataPromise.then(function(data) {
+        console.log(data)
+        processInfo(data)
+    })
+    console.log("Request sent off...")
+}
+
+function processInfo(getInfo) {
+
+    let postsUl = document.getElementById("posts")
+
+    postsUl.innerText = getInfo.value
+}
